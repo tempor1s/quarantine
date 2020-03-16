13f7eff92a726dec4a152a6cfbc6a2229490818b
Ben <benlaugherty@gmail.com>
2020-03-16 16:49:30 -0600

    found a way around env variables

--- web/src/apis/quarantine.ts
+++ web/src/apis/quarantine.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL =
+    process.env.REACT_APP_BASE_URL || 'https://quarantine-api.dev.benlafferty.me';
 
 export default axios.create({
-    baseURL: 'https://quarantine-api.dev.benlafferty.me'
+    baseURL: BASE_URL
 });
