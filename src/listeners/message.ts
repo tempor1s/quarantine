b521228d9de6c65645393ad818cf44d0fd6a2ae4
Ben <benlaugherty@gmail.com>
2020-11-05 17:54:28 -0500

    :(

--- src/listeners/message.ts
+++ src/listeners/message.ts
@@ -1,22 +1,22 @@
-import { Listener } from 'discord-akairo'
-import { TextChannel, Message} from 'discord.js'
-
-// AudiListener will react to every message that audi sends with reactions
-export default class AudiListener extends Listener {
-    public constructor() {
-        super('Audi Meme Machine', {
-            emitter: 'client',
-            event: 'message',
-            category: 'client',
-        })
-    }
-
-    // react :)
-    public async exec(message: Message): Promise<void> {
-        // only react if the author is audi
-        if (message.author.id === "106975614895955968") {
-            await message.react("746163149245513809") // audi
-            await message.react("772161673866903575") // peepoPing
-        }
-    }
-}
+// import { Listener } from 'discord-akairo'
+// import { Message } from 'discord.js'
+//
+// // AudiListener will react to every message that audi sends with reactions
+// export default class AudiListener extends Listener {
+//     public constructor() {
+//         super('Audi Meme Machine', {
+//             emitter: 'client',
+//             event: 'message',
+//             category: 'client',
+//         })
+//     }
+//
+//     // react :)
+//     public async exec(message: Message): Promise<void> {
+//         // only react if the author is audi
+//         if (message.author.id === "106975614895955968") {
+//             await message.react("746163149245513809") // audi
+//             await message.react("772161673866903575") // peepoPing
+//         }
+//     }
+// }
